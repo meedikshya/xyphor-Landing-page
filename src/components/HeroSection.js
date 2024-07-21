f89736dae502dd6d9584f35bf6ac5a0655487a95
Dikshya Sharma <dikxu22@gmail.com>
2024-07-21 19:56:40 +0545

    first commit

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -25,7 +25,7 @@ export default function HeroSection() {
           {/* Image Overlay */}
           <div className='absolute inset-0 z-20 flex items-center justify-center'>
             <img
-              src={Image}
+              src='/images/image.png'
               alt='Overlay'
               className='max-h-[200px] max-w-[200px] object-cover'
             />
@@ -40,7 +40,7 @@ export default function HeroSection() {
           xmlns='http://www.w3.org/2000/svg'
           viewBox='0 0 1200 120'
           preserveAspectRatio='none'
-          className='rotate-y-180 h-[294px] w-[calc(166%+1.3px)] transform'
+          className='h-[294px] w-[calc(166%+1.3px)] rotate-y-180 transform'
         >
           <path
             d='M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z'
